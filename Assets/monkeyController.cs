1d731dba6ba33068b59630f1e5b69f2f644ba8c1
charlie <charlie275691502@gmail.com>
2017-11-09 12:05:06 +0800

    init previous work

---
+++ Assets/monkeyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monkeyController : MonoBehaviour {
+	public float speed;
+	public int dir;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(speed, 0);
+		dir = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2(speed * dir, 0);
+			dir = -dir;
+		}
+	}
+}
