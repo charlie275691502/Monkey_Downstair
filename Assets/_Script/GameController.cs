902e0060eec397f4f8f77a9e85dec4978f1ef298
charlie <charlie275691502@gmail.com>
2017-12-08 23:38:00 +0800

    樓梯效果

--- Assets/_Script/GameController.cs
+++ Assets/_Script/GameController.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 	public Camera camera;
+	public Animator a;
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.A)) {
+			a.Play ("jump");
+		}
+	}
 }
--- Assets/_Script/Stair.cs
+++ Assets/_Script/Stair.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum StairType{
+	Basic,
+	Ice,
+	Tomato,
+	Jump,
+	RollLeft,
+	RollRight
+}
+
 public class Stair : MonoBehaviour {
+	public StairType stairType;
 	
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Monkey") {
-			coll.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (coll.gameObject.GetComponent<Rigidbody2D> ().velocity.x, 0);
+			switch (stairType) {
+			case StairType.Jump:
+				coll.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector3 (0, 200, 0));
+				GetComponent<Animator> ().Play ("jump");
+				break;
+			default:
+				break;
+			}
 		}
 	}
 }
